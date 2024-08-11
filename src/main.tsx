13e81425a6d51de7146beb8c7e686b144e444495
Richard Ginzburg <richard@ginzburg.se>
2024-08-11 14:43:40 +0300

    Add MSW

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,12 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import {worker} from "./mocks/browser.ts";
+
+worker.start().then(() => {
+  createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+      <App />
+    </StrictMode>,
+  )
+});
